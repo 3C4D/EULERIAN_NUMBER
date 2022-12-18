ff05941d2b91bb0cbbe030e8ee09d3bb6826b80e
SchizoBoy <cadonienzo@gmail.com>
2022-12-18 16:52:24 +0100

    programme principal

--- src/main.c
+++ src/main.c
@@ -3,14 +3,26 @@
 #include "euler.h"
 
 int main(int argc, char **argv){
-  int i, j;
+  int n, m;
 
-  for(i = 1; i < atoi(argv[1])+1; i++){
-    for(j = 0; j < i; j++){
-      printf("%d ", nombre_eulerien(i, j));
-    }
-    putchar('\n');
+  if(argc < 3){
+    fprintf(stderr, "\nUsage : %s n m\n", argv[0]);
+    fprintf(stderr, "    - n, m > 0\n");
+    fprintf(stderr, "    - m < n\n\n");
+    exit(-1);
   }
+
+  n = atoi(argv[1]);
+  m = atoi(argv[2]);
+
+  if(n < 1 || m < 0 || m > n-1){
+    fprintf(stderr, "\nUsage : %s n m\n", argv[0]);
+    fprintf(stderr, "    - n, m > 0\n");
+    fprintf(stderr, "    - m < n\n\n");
+    exit(-1);
+  }
+
+  printf("\nA(%d, %d) = %d\n\n", n, m, nombre_eulerien(n, m));
 
   exit(0);
 }
